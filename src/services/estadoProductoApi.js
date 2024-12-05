7c467991053c990f00a7924ffb2e3de8f0f1348f
Jvargast <jose.vargas.t@csi.cl>
2024-12-05 16:44:46 -0300

    cambios para deploy

--- src/services/estadoProductoApi.js
+++ src/services/estadoProductoApi.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_URL } from "./apiBase";
 
 export const estadoProductoApi = createApi({
   reducerPath: "estadoProductoApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_BASE_URL + "/estados-productos", // Asegúrate de tener configurada esta variable
+    baseUrl: process.env.REACT_APP_BASE_URL ?  process.env.REACT_APP_BASE_URL : API_URL + "/estados-productos", // Asegúrate de tener configurada esta variable
     credentials: "include", // Para enviar cookies si es necesario
   }),
   tagTypes: ["EstadosProductos"], // Para manejar la invalidación de caché
